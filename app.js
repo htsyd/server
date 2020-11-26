338503f8752401fcd113f49e4c9b45066588da42
reyhanprtrs <reyhanprtrs@gmail.com>
2020-11-27 00:11:38 +0700

    1. add register on controller 2. add errorhandler feature 3. add hooks on model 4. add validate on model

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const app = express();
 const port = 3000;
 const cors = require('cors');
 const MainRouter = require('./routers')
+const errorHandler = require('./middlewares/errorhandler')
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
-
-
 app.use('/',MainRouter)
+app.use(errorHandler)
 
 app.listen(port,()=>{
   console.log(port);
