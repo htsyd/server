adf9c9df9bd3219c9181bb8263e90d02c28aa038
okkyfauzan <okkypratama48@gmail.com>
2020-11-27 00:18:22 +0700

    menambahkan fitur login d server

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,41 @@
 const { User } = require('../models')
+const PasswordHelper = require('../helpers/passwordhelper')
+const JwtHelper = require('../helpers/jwthelper')
 
 class Controller {
 
-  static login(res, req, next) {
-
+  static async login(req, res, next) {
+    try {
+      const user = await User.findOne({
+        where: {
+          email: req.body.email
+        }
+      })
+      if (!user) {
+        throw {
+          status: 400,
+          message: "Invalid Email/Password"
+        }
+      } else if (PasswordHelper.comparePassword(req.body.password, user.password)) {
+        const object = {
+          id: user.id,
+          email: user.email,
+          name: user.name
+        }
+        const access_token = JwtHelper.generateToken(object)
+        res.status(200).json({ access_token })
+      } else {
+        throw {
+          status: 400,
+          message: "Invalid Email/Password"
+        }
+      }
+    }
+    catch (err) {
+      next(err)
+    }
   }
+
   static register(res, req, next) {
 
   }
